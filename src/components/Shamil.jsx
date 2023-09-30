817ad277b0b254e97d676e276f2b5ae866f6b452
abilov042 <shamilabilov599@gmail.com>
2023-09-30 11:37:11 +0400

    bug fix

--- src/components/Shamil.jsx
+++ src/components/Shamil.jsx
@@ -1,67 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Stomp from 'stompjs';
 
-const Shamil = () => {
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState('');
-  const [username, setUsername] = useState('');
+const ChatApp = () => {
   const [stompClient, setStompClient] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
 
   useEffect(() => {
-    // STOMP istemcisini oluştur
-    const socket = new WebSocket('ws://localhost:8080/chat',); // WebSocket sunucusunun URL'sini ayarlayın
-    const stomp = Stomp.over(socket);
+    const socket = new WebSocket('ws://localhost:8080/chat');
+    const client = Stomp.over(socket);
+    client.connect({}, (frame) => {
+      console.log('STOMP bağlantısı başarıyla açıldı.');
+      setStompClient(client);
 
-    // WebSocket açıldığında
-    socket.addEventListener('open', (event) => {
-      console.log('WebSocket bağlantısı başarıyla açıldı.');
-
-      // STOMP istemcisini başlat
-      stomp.connect({}, (frame) => {
-        console.log('STOMP bağlantısı başarıyla açıldı.');
-        setStompClient(stomp);
+      client.subscribe('/topic/message', (message) => {
+        const receivedData = JSON.parse(message.body);
+        setMessages((prevMessages) => [...prevMessages, receivedData.message]);
       });
     });
 
-    // Mesaj alındığında
-    stomp.connect({}, (frame) => {
-      console.log("Aqil")
-      stomp.subscribe('/topic/message', (message) => {
-        console.log(message);
-        const receivedData = JSON.parse(message.message);
-        setMessages((prevMessages) => [...prevMessages, receivedData]);
-      });
-    });
-
-    // WebSocket kapanırsa
-    socket.addEventListener('close', (event) => {
-      console.log('WebSocket bağlantısı kapatıldı.');
-
-      // STOMP istemcisini kapat
-      if (stompClient) {
-        stompClient.disconnect();
-      }
-    });
-
-    // Komponentin temizlenmesi sırasında bağlantıları kapat
     return () => {
       if (stompClient) {
         stompClient.disconnect();
-      }
-      if (socket) {
-        socket.close();
       }
     };
   }, []);
 
   const sendMessage = () => {
-    if (stompClient && message.trim() !== '') {
+    if (!messageInput || messageInput.trim() === '') {
+      return; // Boş mesaj gönderme işlemini engelle
+    }
+
+    if (stompClient) {
       const data = {
-        //username: username,
-        message: message,
+        message: messageInput,
       };
       stompClient.send('/chat', {}, JSON.stringify(data));
-      setMessage('');
+      setMessageInput('');
     }
   };
 
@@ -70,26 +45,18 @@ const Shamil = () => {
       <h1>React WebSocket Chat</h1>
       <div className="message-box">
         {messages.map((msg, index) => (
-          <p key={index}>
-            <strong>{msg.username}:</strong> {msg.message}
-          </p>
+          <p key={index}>{msg}</p>
         ))}
       </div>
       <input
         type="text"
-        placeholder="Kullanıcı Adınız"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
         placeholder="Mesajınızı yazın"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
+        value={messageInput}
+        onChange={(e) => setMessageInput(e.target.value)}
       />
       <button onClick={sendMessage}>Gönder</button>
     </div>
   );
 };
 
-export default Shamil;
+export default ChatApp;
